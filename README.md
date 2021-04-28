0473e064d3158ce3effcf3249edc1c5b4db93cf7
seyed-mostafa <mostafa.atyabi.1380@gmail.com>
2021-04-29 02:16:48 +0430

    created project

---
+++ README.md
@@ -0,0 +1,3 @@
+# ap_project_1400
+ flutter_food_ordering_app 
+
---
+++ lib/run_test.dart
@@ -0,0 +1,21 @@
+
+
+// ye main joda baraye test clss ha
+
+import 'package:flutter/material.dart';
+
+
+
+void main() {
+  runApp(Ap());
+}
+
+
+class Ap extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
