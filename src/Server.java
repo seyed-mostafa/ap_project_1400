6d189e2d90498775daea76812838e635de7e0af0
seyed-mostafa <mostafa.atyabi.1380@gmail.com>
2021-06-24 15:07:00 +0430

    انتقال دیتا رستوران

--- src/Server.java
+++ src/Server.java
@@ -386,6 +386,34 @@ class ClientHandler implements Runnable {
 //        data+="&";
 
 
+
+
+        ////////////////////////////////////                                   restaurants data                         /////////////////////////////////
+
+
+        for (Restaurant restaurant:restaurants) {
+            data+=  restaurant.getName()+"^"+
+                    restaurant.getAddress().getAddress()+"^"+
+                    restaurant.getAddress().getLongitude()+"^"+
+                    restaurant.getAddress().getLatitude() +"^"+
+                    restaurant.getPhoneNumber()+"^"+
+                    restaurant.getPassword()+"^"+
+                    restaurant.getSendingRangeRadius()+"^"+
+                    restaurant.getId()+"^"+
+                    restaurant.getDays()+"^"+
+                    restaurant.getHour()+"^"+
+                    restaurant.getDays()+"^"+
+
+
+            ;
+            for (Food.TypeFood typeFood:restaurant.getTypeFoods()) {
+                data+=typeFood+"::";
+            }
+            data+="^";
+
+        }
+
+
         return data;
     }
 
